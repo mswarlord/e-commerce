4a34496d985b63a4fcfa147c4df22b865a44832c
Facundo Cantero Rodriguez <49701336+mswarlord@users.noreply.github.com>
2022-09-07 19:56:22 -0300

    Creación inicial del E-Commerce y logo
    TP1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
-      
+      <Logo />
     </div>
   );
 }
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Logo.css'
+const Logo = () => {
+    return (
+        <>
+            <div className='logo'>
+                <div className='logo1'></div>
+                <div className='logo2'></div>
+                <div className='logo3'></div>
+            </div>
+            <h1 className='titulo'>Tres Bastiones</h1>
+            <h2 className='subtitulo'>3D Prints</h2>
+        </>
+    )
+}
+
+export default Logo;
