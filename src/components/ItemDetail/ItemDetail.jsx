d779f83e8cc46aea29037b251fe4626e223495a4
mswarlord <canterofacundo@hotmail.com>
2022-10-19 00:03:21 -0300

    Intercambiabilidad de componentes
    Se RealizÃ³ la codificaciÃ³n que agrega al carrito el producto e
    intercambia el componente contador por una botonera para finalizar la
    compra o seguir comprando.

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import './ItemDetail.css'
 import ItemCount from '../ItemCount/ItemCount'
-
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 const ItemDetail = ({id, img, nombre, descripcion, precio, material, stock, categoria}) => {
-    
+const [endPurchase, setEndPurchase] = useState(false)
+
     const onAdd = (qty) => {
+        setEndPurchase(true);
+        /* const productToAdd ={
+            id, nombre, precio, qty
+        }
+        console.log(productToAdd); */
         alert(`Agregaste ${qty} productos`);
     };
 
     return (
     <article className='detalleItem'>
+            
             <picture className='imagenDetailProducto'>
                 <img src={img} alt={nombre} />
             </picture>
-            <div className='contadorDetalle'>
-                <ItemCount onAdd={onAdd} initial={1} stock={stock} />
-            </div>
+            
+            
             <div className='detalle'>
                 <div className='tituloProducto'>
                     <h2>{nombre}</h2>
@@ -26,8 +33,21 @@ const ItemDetail = ({id, img, nombre, descripcion, precio, material, stock, cate
                 <h4><span className='detalleAtributo'>Stock:</span> {stock}</h4>
                 <h4><span className='detalleAtributo'>Categoría:</span> {categoria}</h4>
             </div>
+        {
+            endPurchase ?
+            <div className='detalleBotonera'>
+                <NavLink to="/">
+                    <button>Seguir comprando</button>
+                </NavLink>
+                <NavLink to="/cart">
+                    <button>Finalizar compra</button>
+                </NavLink>
+            </div>
+            :
+            <div className='contadorDetalle'> <ItemCount onAdd={onAdd} initial={1} stock={stock} /> </div>
+        } 
     </article>
-  )
+    )
 }
 
 export default ItemDetail;
